8324cccd12684ecb3421caadda3d7b11003929fa
Gabriel Bishop <gbishop12@me.com>
2019-04-26 16:19:46 -0400

    fix db

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
-const heroku = 'http://localhost:5000';
+const heroku = 'https://inventory-tracker-angular.herokuapp.com/';
 
 const httpOptions = {
   headers: new HttpHeaders(),
